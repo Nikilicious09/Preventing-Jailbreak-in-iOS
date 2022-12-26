bb09fbc20ef5be76f69790c86fce2b92fa27fb51
Nikhil Vinod <nikhilvinodmec@gmail.com>
2022-12-26 14:01:54 +0530

    Create SecurityUtils.swift

---
+++ SecurityUtils.swift
@@ -0,0 +1,97 @@
+class SecurityUtils {
+    
+    static var isSimulator: Bool {
+        #if targetEnvironment(simulator)
+            return true
+        #else
+            return false
+        #endif
+    }
+    
+    static var isOnMac: Bool {
+        if #available(iOS 14.0, *), ProcessInfo.processInfo.isiOSAppOnMac {
+            return true
+        }
+        return false
+    }
+    
+    static var isJailBroken: Bool {
+        get {
+            if TPJailBreakUtils.hasCydiaInstalled() { return true }
+            if TPJailBreakUtils.isContainsSuspiciousApps() { return true }
+            if TPJailBreakUtils.isSuspiciousSystemPathsExists() { return true }
+            return TPJailBreakUtils.canEditSystemFiles()
+        }
+    }
+}
+
+
+struct TPJailBreakUtils {
+        
+    static func hasCydiaInstalled() -> Bool {
+        return UIApplication.shared.canOpenURL(URL(string: "cydia://")!)
+    }
+    
+    static func isContainsSuspiciousApps() -> Bool {
+        for path in suspiciousAppsPathToCheck {
+            if FileManager.default.fileExists(atPath: path) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    static func isSuspiciousSystemPathsExists() -> Bool {
+        for path in suspiciousSystemPathsToCheck {
+            if FileManager.default.fileExists(atPath: path) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    static func canEditSystemFiles() -> Bool {
+        let jailBreakText = "Developer Insider"
+        do {
+            try jailBreakText.write(toFile: jailBreakText, atomically: true, encoding: .utf8)
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    static var suspiciousAppsPathToCheck: [String] {
+        return ["/Applications/Cydia.app",
+                "/Applications/blackra1n.app",
+                "/Applications/FakeCarrier.app",
+                "/Applications/Icy.app",
+                "/Applications/IntelliScreen.app",
+                "/Applications/MxTube.app",
+                "/Applications/RockApp.app",
+                "/Applications/SBSettings.app",
+                "/Applications/WinterBoard.app"
+        ]
+    }
+    
+    static var suspiciousSystemPathsToCheck: [String] {
+        return ["/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+                "/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+                "/private/var/lib/apt",
+                "/private/var/lib/apt/",
+                "/private/var/lib/cydia",
+                "/private/var/mobile/Library/SBSettings/Themes",
+                "/private/var/stash",
+                "/private/var/tmp/cydia.log",
+                "/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+                "/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+                "/usr/bin/sshd",
+                "/usr/libexec/sftp-server",
+                "/usr/sbin/sshd",
+                "/etc/apt",
+                "/bin/bash",
+                "/Library/MobileSubstrate/MobileSubstrate.dylib"
+        ]
+    }
+
+}
+
